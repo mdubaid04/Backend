05feec77f56badd8d8e38ec4c1d2340d612e40cb
mdubaid04 <mdubaid012@gmail.com>
2026-05-23 10:08:19 +0530

    configure controller and route

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// import routes
+import userRouter from "./routes/user.routes.js";
+
+//declaration of routes
+
+app.use("/api/v1/user", userRouter);
+
 export { app };
